9ca918bc9a4e6ded3d39324687ec05d9e5b30619
Séga SYLLA <sega.sylla.biz@gmail.com>
2024-02-24 15:14:02 +0100

    CRUD fournisseurs OK

---
+++ src/main/java/fr/epsi/poec24/demospring/controller/ArticleController.java
@@ -0,0 +1,9 @@
+package fr.epsi.poec24.demospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/articles")
+public class ArticleController {
+}
